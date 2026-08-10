7f96bb26b1fb127d5471a123fbf2a0768ad49361
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-08-10 23:51:53 +0530

    k8

---
+++ 60-acm/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "kkf-s3-testing-remote-state-dev"
+    key    = "roboshop-dev-acm"
+    region = "us-east-1"
+    dynamodb_table = "kkf-s3-testing"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options"
+}
