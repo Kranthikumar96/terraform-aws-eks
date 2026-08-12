9c16692a3c2bb86e230a10bfa153fb936aa2ed5c
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-08-12 12:19:39 +0530

    k8

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -49,6 +49,21 @@ module "eks" {
         AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
       }
     }
+    green = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.xlarge"]
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      iam_role_additional_policies = {
+        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+        AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+      }
+    }
   }
 
   tags = merge(
