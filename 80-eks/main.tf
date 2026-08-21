8ad0edf601d452a9008fe02f6c0b2ce26161e098
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-08-21 10:47:28 +0530

    jenkins

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -49,7 +49,7 @@ module "eks" {
         AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
       }
     }
-    /* green = {
+    green = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.xlarge"]
@@ -70,7 +70,7 @@ module "eks" {
           effect = "NO_SCHEDULE"
         }
       }
-    } */
+    }
   }
 
   tags = merge(
